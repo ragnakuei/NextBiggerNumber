d13d8ba3d236a79c7d4d1cea16025970ff746153
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-02-03 20:19:02 +0800

    1st red light

---
+++ NextBiggerNumberKata/NextBiggerNumberKata/NextBiggerNumberTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NextBiggerNumberKata
+{
+    [TestClass]
+    public class NextBiggerNumberTest
+    {
+        [TestMethod]
+        public void NextBiggerNumber_input_12_should_be_21()
+        {
+            // arrange
+            var input = 12;
+            var expected = 21;
+
+            // act
+            var actual = Kata.NextBiggerNumber(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Assert.AreEqual(531, Kata.NextBiggerNumber(513));
+        //Assert.AreEqual(2071, Kata.NextBiggerNumber(2017));
+        //Assert.AreEqual(441, Kata.NextBiggerNumber(414));
+        //Assert.AreEqual(414, Kata.NextBiggerNumber(144));
+    }
+
+    internal class Kata
+    {
+        internal static int NextBiggerNumber(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
